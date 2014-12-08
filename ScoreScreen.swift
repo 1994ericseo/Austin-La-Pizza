b8296cb237219828d61fe850a032ea568272f2f1
kavin12345 <seokev@gmail.com>
2014-12-08 02:07:53 -0800

    Updated ScoreScreen.swift
    Updated replayButton, goHomeButton, gameOverButton, background, few
    other methods

--- ScoreScreen.swift
+++ ScoreScreen.swift
@@ -10,7 +10,28 @@ import SpriteKit
 
 class ScoreScreen: SKScene {
     var score = 0
+    let replayButton = SKSpriteNode(imageNamed: "Replay")
+    let goHomeButton = SKSpriteNode(imageNamed: "Home")
+    let gameOverBanner = SKSpriteNode(imageNamed: "GameOver")
+    let background = PlayScene()
     
+    override func didMoveToView(view: SKView) {
+        self.backgroundColor = UIColor.whiteColor()
+        
+        self.replayButton.size = CGSizeMake(50, 50)
+        self.replayButton.position = CGPointMake(CGRectGetMidX(self.frame)-30, 50)
+        
+        self.gameOverBanner.size = CGSizeMake(200, 200)
+        self.gameOverBanner.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) + 80)
+        
+        self.goHomeButton.size = CGSizeMake(50, 50)
+        self.goHomeButton.position = CGPointMake(CGRectGetMidX(self.frame)+30, 50)
+        
+        
+        self.addChild(gameOverBanner)
+        self.addChild(goHomeButton)
+        self.addChild(replayButton)
+    }
 
     // To set the high score
     func newHighScore(score: Int) {
@@ -21,4 +42,32 @@ class ScoreScreen: SKScene {
             userDefaults.synchronize()
         }
     }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self) /* location of touch */
+            
+            if self.nodeAtPoint(location) == self.replayButton {
+                background.size = self.size
+                let skView = self.view
+                skView?.ignoresSiblingOrder = true
+                background.scaleMode = .ResizeFill
+                
+                
+                skView?.presentScene(background)
+            }
+            
+            else if self.nodeAtPoint(location) == goHomeButton {
+                background.size = self.size
+                let skView = GameScene()
+                background.scaleMode = .ResizeFill
+                
+            }
+        }
+        
+    }
+
+    
 }
+
+
