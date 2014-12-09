c1e84f4b229185ad1cb3a32b16788a5d97eb6db9
kavin12345 <seokev@gmail.com>
2014-12-08 21:13:09 -0800

    Added content in HighScoreScreen.swift
    goHomeButton, highScoreBanner

--- Austin La Pizza/HighScoreScreen.swift
+++ Austin La Pizza/HighScoreScreen.swift
@@ -7,3 +7,45 @@
 //
 
 import SpriteKit
+
+class HighScoreScreen: SKScene {
+    var score = 0
+    let goHomeButton = SKSpriteNode(imageNamed: "Home")
+    let highScoreBanner = SKSpriteNode(imageNamed: "HighScore")
+    
+    
+    override func didMoveToView(view: SKView) {
+        self.backgroundColor = UIColor.whiteColor()
+        
+        self.highScoreBanner.size = CGSizeMake(200, 200)
+        self.highScoreBanner.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) + 80)
+        
+        self.goHomeButton.size = CGSizeMake(50, 50)
+        self.goHomeButton.position = CGPointMake(CGRectGetMidX(self.frame)+30, 50)
+        
+        self.addChild(highScoreBanner)
+        self.addChild(goHomeButton)
+    }
+    
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self) /* location of touch */
+            if self.nodeAtPoint(location) == goHomeButton {
+                var menuBackground = GameScene()
+                menuBackground.size = self.size
+                let skView = self.view
+                skView?.ignoresSiblingOrder = true
+                menuBackground.scaleMode = .ResizeFill
+                
+                var reveal = SKTransition.fadeWithColor(UIColor.whiteColor(), duration: 0.5)
+                
+                skView?.presentScene(menuBackground, transition: reveal)
+                
+            }
+        }
+        
+    }
+    
+    
+}
